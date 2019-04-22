aff98298da151e6f4b7b3b804a93a206df606952
cbethune <cbethune@uncharted.software>
2019-04-22 14:19:44 -0400

    completes timeseries pipeline and and robustness to scoring

--- processing/pipelines/timeseries.py
+++ processing/pipelines/timeseries.py
@@ -11,6 +11,8 @@ from d3m.metadata import hyperparams
 
 from exline.primitives.simple_column_parser import SimpleColumnParserPrimitive
 from exline.primitives.ragged_dataset_loader import RaggedDatasetLoaderPrimitive
+from exline.primitives.timeseries_reshaper import TimeSeriesReshaperPrimitive
+from exline.primitives.timeseries_neighbours import TimeSeriesNeighboursPrimitive
 
 from common_primitives.denormalize import DenormalizePrimitive
 from common_primitives.dataset_to_dataframe import DatasetToDataFramePrimitive
@@ -33,28 +35,39 @@ def create_pipeline(metric: str) -> Pipeline:
     step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
     step.add_output('produce')
     step.add_output('produce_collection')
+    step.add_hyperparameter('sample', ArgumentType.VALUE, 1.0)
     ts_pipeline.add_step(step)
 
-    # Append column parser.  D3M dataset loader creates a dataframe with all columns set to 'object', this pipeline is
+    # step 1 - Append column parser.  D3M dataset loader creates a dataframe with all columns set to 'object', this pipeline is
     # designed to work with string/object, int, float, boolean.  This also shifts the d3mIndex to be the dataframe index and
     # drop the target.
     step = PrimitiveStep(primitive_description=SimpleColumnParserPrimitive.metadata.query())
-    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=input_val.format(previous_step))
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
     step.add_output('produce')
     step.add_output('produce_target')
     ts_pipeline.add_step(step)
-    previous_step += 1
 
-    # time series classifier primitive
+    # step 2 - Reformats timeseries data if sparse, truncates if required
+    step = PrimitiveStep(primitive_description=TimeSeriesReshaperPrimitive.metadata.query())
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce_collection')
+    step.add_output('produce')
+    ts_pipeline.add_step(step)
 
-    # convert predictions to expected format
+    # step 3 - Classification / regression of series data
+    step = PrimitiveStep(primitive_description=TimeSeriesNeighboursPrimitive.metadata.query())
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+    step.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce_target')
+    step.add_output('produce')
+    step.add_hyperparameter('metric', ArgumentType.VALUE, metric)
+    ts_pipeline.add_step(step)
+
+    # step 4 - convert predictions to expected format
     step = PrimitiveStep(primitive_description=ConstructPredictionsPrimitive.metadata.query())
-    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=input_val.format(previous_step))
-    step.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce_target')
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+    step.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
     step.add_output('produce')
     step.add_hyperparameter('use_columns', ArgumentType.VALUE, [0, 1])
     ts_pipeline.add_step(step)
-    previous_step += 1
 
     # Adding output step to the pipeline
     ts_pipeline.add_output(name='output', data_reference=input_val.format(previous_step))
