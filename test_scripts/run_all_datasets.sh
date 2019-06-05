efe7052aa2a353c7fbf2603a8752d6020308fede
fred <fred.battista@qntfy.com>
2019-06-05 14:43:30 -0700

    WIP

---
+++ test_scripts/run_all_datasets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get up to get up
+cd ..
+
+# Start TA2
+docker-compose up -d distil 
+
+# List all the datasets there be in seed_datasets
+for dir in seed_datasets_current/*
+do
+    export DATASET=`python -c "a = '$dir'; print(a.split('/')[1]);"`
+    echo $DATASET
+    docker-compose up dummy-ta3
+    echo $DATASET
+done
