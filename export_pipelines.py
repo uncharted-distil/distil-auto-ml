36b4e818f8c218f23401d56e953f64512a7cde9a
fred <fred.battista@qntfy.com>
2019-06-06 13:28:57 -0700

    Deleting old pipes

--- export_pipelines.py
+++ export_pipelines.py
@@ -13,19 +13,20 @@ l.addHandler(logging.NullHandler())
 META_DIR = 'pipelines'
 
 # Map of default datasets to configure .meta files
+# and metric for pipeline config
 PIPE_TO_DATASET = {
-    'tabular': '185_baseball',
-    'audio': '31_urbansound',
-    'clustering': '1491_one_hundred_plants_margin_clust',
-    'collaborative_filtering': '60_jester',
-    'community_detection': '6_70_com_amazon',
-    'graph_matching': '49_facebook',
-    'image': '22_handgeometry',
-    'link_prediction': '59_umls',
-    'question_answer': '32_wikiqa',
-    'text': '30_personae',
-    'timeseries': '56_sunspots_monthly',
-    'vertex_nomination': 'LL1_net_nomination_seed',
+    'tabular': ('185_baseball', 'f1Macro'),
+    'audio': ('31_urbansound', 'accuracy'),
+    'clustering': ('1491_one_hundred_plants_margin_clust', 'normalizedMutualInformation'),
+    'collaborative_filtering': ('60_jester', 'meanAbsoluteError'),
+    'community_detection': ('6_70_com_amazon', 'normalizedMutualInformation'),
+    'graph_matching': ('49_facebook', 'accuracy'),
+    'image': ('22_handgeometry', 'meanSquaredError'),
+    'link_prediction': ('59_umls', 'accuracy'),
+    'question_answer': ('32_wikiqa', 'f1'),
+    'text': ('30_personae', 'f1'),
+    'timeseries': ('56_sunspots_monthly', 'rootMeanSquaredError'),
+    'vertex_nomination': ('LL1_net_nomination_seed', 'accuracy'),
 }
 
 # Skeleton of .meta data
@@ -68,13 +69,13 @@ if __name__ == '__main__':
         lib = importlib.import_module('processing.pipelines.' + p)
         try:
             # Get the pipeline json and id, write it out
-            pipe_json = lib.create_pipeline(metric='f1Macro').to_json_structure()
+            dataset_to_use, metric = PIPE_TO_DATASET[p]
+            pipe_json = lib.create_pipeline(metric=metric).to_json_structure()
             id = pipe_json['id']
             with open(os.path.join(META_DIR, id + '.json'), 'w') as f:
                     f.write(json.dumps(pipe_json, indent=4))
                     f.write('\n')
             # Get meta alongside pipeline
-            dataset_to_use = PIPE_TO_DATASET[p]
             meta = set_meta(dataset_to_use)
             with open(os.path.join(META_DIR, id + '.meta'), 'w') as f:
                 f.write(json.dumps(meta, indent=4))
