4600c217a4ba2a89ff29dbffbe8879f184b5da55
cbethune <cbethune@uncharted.software>
2019-06-15 02:22:22 -0400

    ensures only one pipeline is submitted per primitive

--- submit_all_annotations.py
+++ submit_all_annotations.py
@@ -4,6 +4,13 @@ import glob
 import json
 import shutil
 
+
+def clean_pipelines(folder_path):
+    pathd = os.path.join(folder_path, 'pipelines')
+    if os.path.isdir(pathd):
+        print(f'Deleting {pathd}')
+        shutil.rmtree(pathd)
+
 # Load the pipelines & meta files
 pipeline_files = glob.glob('pipelines/*.json')
 pipelines = {}
@@ -38,15 +45,22 @@ for p in primitive_files:
     dir_name, _ = os.path.split(primitive_path)
     os.makedirs(dir_name, exist_ok=True)
     shutil.copy(p, primitive_path)
+
+    # clean out existing pipelines to avoid accumulating
+    clean_pipelines(folder_path)
+
     for k,v in pipelines.items():
         for s in v['steps']:
             pipe_prim = s['primitive']['python_path']
             if pipe_prim == key:
-                old_pipeline_path = [p for p in pipeline_files if k in p][0]
-                new_pipeline_path = os.path.join(folder_path, 'pipelines', k)
-                old_meta_path = [p for p in meta_files if k.replace('.json', '') in p][0]
-                new_meta_path = os.path.join(folder_path, 'pipelines', k.replace('json', 'meta'))
-                dir_name, _ = os.path.split(new_pipeline_path)
-                os.makedirs(dir_name, exist_ok=True)
-                shutil.copy(old_pipeline_path, new_pipeline_path)
-                shutil.copy(old_meta_path, new_meta_path)
+                    old_pipeline_path = [p for p in pipeline_files if k in p][0]
+                    new_pipeline_path = os.path.join(folder_path, 'pipelines', k)
+                    old_meta_path = [p for p in meta_files if k.replace('.json', '') in p][0]
+                    new_meta_path = os.path.join(folder_path, 'pipelines', k.replace('json', 'meta'))
+                    dir_name, _ = os.path.split(new_pipeline_path)
+                    os.makedirs(dir_name, exist_ok=True)
+                    pipeline_root_path = os.path.join(folder_path, 'pipelines')
+                    # only copy one pipeline file for any given primitive
+                    if os.path.isdir(pipeline_root_path) and not os.listdir(pipeline_root_path):
+                        shutil.copy(old_pipeline_path, new_pipeline_path)
+                        shutil.copy(old_meta_path, new_meta_path)
