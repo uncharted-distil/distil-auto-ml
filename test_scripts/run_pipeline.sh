c708f5cd34804d91daf5b620e2cd34f296a7c6d4
cbethune <cbethune@uncharted.software>
2019-07-05 22:58:30 -0400

    adds eval-style pipeline runner

---
+++ test_scripts/run_pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# arg 1 - directory where pipeline resides
+# arg 2 - pipeline ID (don't include .json / .meta)
+# arg 3 - dataset directory
+
+docker run \
+    --rm \
+    --env D3MINPUTDIR=$1 \
+    -v $1:$1 \
+    -v $3:$3 \
+    registry.datadrivendiscovery.org/jpl/docker_images/complete:ubuntu-bionic-python36-v2019.6.7 \
+    python3 -m d3m runtime -v $D3MSTATICDIR -d $D3MINPUTDIR fit-score -m $1/$2.meta -p $1/$2.json
