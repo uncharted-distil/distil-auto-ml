4c3fb3df0efdd578a814e128dc3766b0538ff082
slangevi <scott.langevin@gmail.com>
2019-07-15 17:14:27 -0700

    Improved semi-supervised pipeline for tabular classification

--- processing/pipelines/semisupervised_tabular.py
+++ processing/pipelines/semisupervised_tabular.py
@@ -14,54 +14,60 @@ def create_pipeline(metric: str) -> Pipeline:
     step_0.add_output('produce')
     pipeline_description.add_step(step_0)
 
-    # Step 1: DISTIL/NK Storc primitive
-    step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.clustering.hdbscan.Hdbscan'))
-    step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
-    step_1.add_hyperparameter(name='long_format', argument_type= ArgumentType.VALUE, data=True)
-    step_1.add_output('produce')
+    # Step 1, 2 column parser
+    step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.DataFrameCommon'))
     pipeline_description.add_step(step_1)
 
-    # Step 2: column parser
-    step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.DataFrameCommon'))
-    step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')                      
+    step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.operator.dataset_map.DataFrameCommon'))
+    step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+    step_2.add_hyperparameter(name='primitive', argument_type= ArgumentType.PRIMITIVE, data=1)
     step_2.add_output('produce')
     pipeline_description.add_step(step_2)
-    
-    # Step 3: imputer
+
+    # Step 3,4: imputer
     step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
-    step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
     step_3.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE,data='replace')
     step_3.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
-    step_3.add_output('produce')
     pipeline_description.add_step(step_3)
 
-    # Step 4,5,6: Distil ensemble classifier
-    step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon'))
-    step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+    step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.operator.dataset_map.DataFrameCommon'))
+    step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+    step_4.add_hyperparameter(name='primitive', argument_type= ArgumentType.PRIMITIVE, data=3)
     step_4.add_output('produce')
     pipeline_description.add_step(step_4)
 
-    step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon'))
-    step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
-    step_5.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,data=('https://metadata.datadrivendiscovery.org/types/TrueTarget',))
+    # Step 5: DISTIL/NK Storc primitive
+    step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.clustering.hdbscan.Hdbscan'))
+    step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
+    step_5.add_hyperparameter(name='long_format', argument_type= ArgumentType.VALUE, data=True)
     step_5.add_output('produce')
     pipeline_description.add_step(step_5)
 
-    step_6 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.learner.random_forest.DistilEnsembleForest'))
-    step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
-    step_6.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
-    step_6.add_hyperparameter('metric', ArgumentType.VALUE, metric)
+    # Step 6,7,8: Distil ensemble classifier
+    step_6 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon'))
+    step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
     step_6.add_output('produce')
     pipeline_description.add_step(step_6)
 
-    # Step 7: construct output
-    step_7 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon'))
-    step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
-    step_7.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+    step_7 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon'))
+    step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+    step_7.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,data=('https://metadata.datadrivendiscovery.org/types/Target',))
     step_7.add_output('produce')
     pipeline_description.add_step(step_7)
 
-    # Final Output
-    pipeline_description.add_output(name='output predictions', data_reference='steps.7.produce')
+    step_8 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.learner.random_forest.DistilEnsembleForest'))
+    step_8.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
+    step_8.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.7.produce')
+    step_8.add_output('produce')
+    pipeline_description.add_step(step_8)
 
+    # Step 9, 10: construct output
+    step_9 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon'))
+    step_9.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.8.produce')
+    step_9.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+    step_9.add_output('produce')
+    pipeline_description.add_step(step_9)
+
+    # Final Output
+    pipeline_description.add_output(name='output predictions', data_reference='steps.9.produce')
     return pipeline_description
