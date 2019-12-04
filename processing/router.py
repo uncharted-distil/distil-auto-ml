c915ec678a4d7662dae0e96e6dec0e0071e9d012
Balazs Horanyi <balazs.horanyi@qntfy.com>
2019-12-03 19:14:14 -0700

    fix typo

--- processing/router.py
+++ processing/router.py
@@ -125,8 +125,8 @@ def is_semisupervised_tabular(problem: dict) -> bool:
 
 def is_data_augmentation_tabular(dataset_doc: dict, problem: dict) -> bool:
     is_data_aug = True if 'data_augmentation' in problem else False
-    is_classification_regression = len(set(_problem['problem']['task_keywords'])
-                                       in set(
+    is_classification_regression = len(set(problem['problem']['task_keywords'])
+                                       & set(
         [_problem.TaskKeyword.REGRESSION, _problem.TaskKeyword.CLASSIFICATION])) > 0
     is_tabular = ['table'] == get_resource_types(dataset_doc)
     return is_data_aug and is_classification_regression and is_tabular
