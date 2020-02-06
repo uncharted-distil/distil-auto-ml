d5a714d6138352106ef9ca88ebb304a527f61cbb
Balazs Horanyi <balazs.horanyi@qntfy.com>
2020-02-06 09:16:28 -0500

    fix var pipeline

--- processing/pipelines/timeseries_var.py
+++ processing/pipelines/timeseries_var.py
@@ -8,6 +8,7 @@ from d3m import container, utils
 from d3m.metadata.pipeline import Pipeline, PrimitiveStep, Resolver
 from d3m.metadata.base import ArgumentType
 from d3m.metadata import hyperparams
+from common_primitives.extract_columns_semantic_types import ExtractColumnsBySemanticTypesPrimitive
 
 from d3m.primitives.time_series_forecasting.vector_autoregression import VAR
 from common_primitives.dataset_to_dataframe import DatasetToDataFramePrimitive
@@ -39,7 +40,8 @@ def create_pipeline(metric: str, resolver: Optional[Resolver] = None) -> Pipelin
 
     # step 1 - Parse columns.
     step = PrimitiveStep(primitive_description=ColumnParserPrimitive.metadata.query(), resolver=resolver)
-    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=input_val.format(previous_step))
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER,
+                      data_reference=input_val.format(previous_step))
     step.add_output('produce')
     semantic_types = ('http://schema.org/Boolean', 'http://schema.org/Integer', 'http://schema.org/Float',
                       'https://metadata.datadrivendiscovery.org/types/FloatVector', 'http://schema.org/DateTime')
@@ -48,10 +50,36 @@ def create_pipeline(metric: str, resolver: Optional[Resolver] = None) -> Pipelin
     previous_step += 1
     parse_step = previous_step
 
+    # Extract attributes
+    step = PrimitiveStep(primitive_description=ExtractColumnsBySemanticTypesPrimitive.metadata.query(),
+                         resolver=resolver)
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=input_val.format(parse_step))
+    step.add_output('produce')
+    step.add_hyperparameter('semantic_types', ArgumentType.VALUE,
+                            ('https://metadata.datadrivendiscovery.org/types/Attribute',
+                             'https://metadata.datadrivendiscovery.org/types/PrimaryKey'))
+    var_pipeline.add_step(step)
+    previous_step += 1
+    attributes_step = previous_step
+
+    # Extract targets
+    step = PrimitiveStep(primitive_description=ExtractColumnsBySemanticTypesPrimitive.metadata.query(),
+                         resolver=resolver)
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=input_val.format(parse_step))
+    step.add_output('produce')
+    target_types = ('https://metadata.datadrivendiscovery.org/types/Target',
+                    'https://metadata.datadrivendiscovery.org/types/TrueTarget')
+    step.add_hyperparameter('semantic_types', ArgumentType.VALUE, target_types)
+    var_pipeline.add_step(step)
+    previous_step += 1
+    target_step = previous_step
+
     # step 2 - Vector Auto Regression
     step = PrimitiveStep(primitive_description=VAR.metadata.query(), resolver=resolver)
-    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=input_val.format(parse_step))
-    step.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference=input_val.format(parse_step))
+    step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER,
+                      data_reference=input_val.format(attributes_step))
+    step.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER,
+                      data_reference=input_val.format(target_step))
     step.add_output('produce')
     var_pipeline.add_step(step)
     previous_step += 1
