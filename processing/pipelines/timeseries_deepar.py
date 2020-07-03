8d574acbaa3de50e38ef0a271e23f87d9e24d275
philippe horne <phorne@uncharted.software>
2020-07-03 09:41:07 -0400

    Added quantile hyperparameter to deepar primitive call.

--- processing/pipelines/timeseries_deepar.py
+++ processing/pipelines/timeseries_deepar.py
@@ -92,6 +92,7 @@ def create_pipeline(metric: str, resolver: Optional[Resolver] = None) -> Pipelin
                       data_reference=input_val.format(attribute_step))
     step.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER,
                       data_reference=input_val.format(target_step))
+    step.add_hyperparameter(name='quantiles', argument_type=ArgumentType.VALUE, data=[0.05, 0.95])
     step.add_output('produce')
     deepar_pipeline.add_step(step)
     previous_step += 1
