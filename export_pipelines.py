305dd6244ec105b93087acde8ea4a1648bec310d
Chris Bethune <cbethune@uncharted.software>
2020-10-09 14:45:34 -0400

    updates dataset name

--- export_pipelines.py
+++ export_pipelines.py
@@ -56,9 +56,9 @@ PIPE_TO_DATASET = {
     'semisupervised_tabular': ('SEMI_1040_sylva_prior_MIN_METADATA', 'f1Macro', {}),
     'timeseries_lstm_fcn': ('LL1_50words_MIN_METADATA', 'f1Macro', {}),
     'data_augmentation_tabular': ('DA_ny_taxi_demand_MIN_METADATA', 'meanAbsoluteError', {}),
-    'remote_sensing': ('big-earth-sample', 'f1Macro', {'svc': False}),
-    'remote_sensing': ('big-earth-sample', 'f1Macro', {'svc': True}),
-    'remote_sensing_mlp': ('locusts-nano', 'f1Macro', {}),
+    'remote_sensing': ('LL1_bigearth_land_use', 'f1Macro', {'svc': False}),
+    'remote_sensing': ('LL1_bigearth_land_use', 'f1Macro', {'svc': True}),
+    'remote_sensing_mlp': ('LL1_bigearth_land_use', 'f1Macro', {}),
     'common': ('LL0_acled_reduced_MIN_METADATA', 'f1Macro', {})
 }
 
